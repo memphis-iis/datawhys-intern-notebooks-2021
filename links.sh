34ac2624257eb8b2036f26cc1b32af4224bde1dd
Andrew Olney <aolney@memphis.edu>
2021-05-30 12:51:02 -0500

    script to generate nbgitpuller links for interns
    includes logging and blockly auto open in split screen

---
+++ links.sh
@@ -0,0 +1,3 @@
+for f in *.ipynb; do 
+    echo "https://jupyter.olney.ai/hub/user-redirect/git-pull?repo=https%3A%2F%2Fgithub.com%2Fmemphis-iis%2Fdatawhys-intern-notebooks-2021&urlpath=lab%2Ftree%2Fdatawhys-intern-notebooks-2021%2F$f%3Flog%3Dhttps%3A%2F%2Flogging.olney.ai%2Fdatawhys%2Flog%26bl%3D1"
+done
